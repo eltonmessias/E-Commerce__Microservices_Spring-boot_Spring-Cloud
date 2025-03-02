38de940dead63c1843cf2d3b50c887fca537e426
elton <eltonguambe1006@gmail.com>
2025-03-02 23:30:11 +0200

    feat: ProductResponse class

--- services/product/src/main/java/com/eltonmessias/ecommerce/product/ProductResponse.java
+++ services/product/src/main/java/com/eltonmessias/ecommerce/product/ProductResponse.java
@@ -1,6 +1,16 @@
 package com.eltonmessias.ecommerce.product;
 
-public record ProductResponse(
+import com.eltonmessias.ecommerce.category.Category;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
+public record ProductResponse(
+        Long id,
+        String name,
+        String description,
+        double price,
+        Integer availableQuantity,
+        Long categoryId,
+        String categoryName
 ) {
 }
