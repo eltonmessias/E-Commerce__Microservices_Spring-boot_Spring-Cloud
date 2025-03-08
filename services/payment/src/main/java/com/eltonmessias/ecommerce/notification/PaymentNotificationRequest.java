a72a9bb6eee7307565161c1a79b5ff1c2039b204
elton <eltonguambe1006@gmail.com>
2025-03-08 07:25:08 +0200

    feat: payment-service -> notification producer

---
+++ services/payment/src/main/java/com/eltonmessias/ecommerce/notification/PaymentNotificationRequest.java
@@ -0,0 +1,15 @@
+package com.eltonmessias.ecommerce.notification;
+
+import com.eltonmessias.ecommerce.payment.PaymentMethod;
+
+import java.math.BigDecimal;
+
+public record PaymentNotificationRequest(
+        String orderReference,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        String customerFirstName,
+        String customerLastName,
+        String customerEmail
+) {
+}
