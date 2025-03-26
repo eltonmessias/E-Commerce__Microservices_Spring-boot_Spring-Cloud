220dff87191f45cea7acaea05679fd80f89a5ca3
elton <eltonguambe1006@gmail.com>
2025-03-26 06:01:30 +0200

    feat: notification service -> consumerOrderConfirmationNotification

--- services/notification/src/main/java/com/eltonmessias/ecommerce/kafka/NotificationConsumer.java
+++ services/notification/src/main/java/com/eltonmessias/ecommerce/kafka/NotificationConsumer.java
@@ -1,9 +1,12 @@
 package com.eltonmessias.ecommerce.kafka;
 
+import com.eltonmessias.ecommerce.email.EmailService;
+import com.eltonmessias.ecommerce.kafka.order.OrderConfirmation;
 import com.eltonmessias.ecommerce.kafka.payment.PaymentConfirmation;
 import com.eltonmessias.ecommerce.notification.Notification;
 import com.eltonmessias.ecommerce.notification.NotificationRepository;
 import com.eltonmessias.ecommerce.notification.NotificationType;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -11,16 +14,19 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
+import static com.eltonmessias.ecommerce.notification.NotificationType.ORDER_CONFIRMATION;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class NotificationConsumer {
 
     private final NotificationRepository repository;
+    private final EmailService emailService;
 
 
     @KafkaListener(topics = "payment-topic")
-    public void consumerPaymentSuccessNotification(PaymentConfirmation paymentConfirmation) {
+    public void consumerPaymentSuccessNotification(PaymentConfirmation paymentConfirmation) throws MessagingException {
         log.info("Consuming the message from payment-topic:: {}", paymentConfirmation);
         repository.save(
                 Notification.builder()
@@ -32,7 +38,35 @@ public class NotificationConsumer {
 
         //send email
         var customerName = paymentConfirmation.customerFirstname() + " " + paymentConfirmation.customerLastname();
+        emailService.sendPaymentSuccessEmail(
+                paymentConfirmation.customerEmail(),
+                customerName,
+                paymentConfirmation.amount(),
+                paymentConfirmation.orderReference()
+        );
+    }
 
+
+    @KafkaListener(topics = "order-topic")
+    public void consumerOrderConfirmationNotification(OrderConfirmation orderConfirmation) throws MessagingException {
+        log.info("Consuming the message from order-topic Topic:: {}", orderConfirmation);
+        repository.save(
+                Notification.builder()
+                        .notificationType(ORDER_CONFIRMATION)
+                        .notificationDate(LocalDateTime.now())
+                        .orderConfirmation(orderConfirmation)
+                        .build()
+        );
+
+        // send email
+        var customerName = orderConfirmation.customer().firstname() + " " + orderConfirmation.customer().lastname();
+        emailService.sendOrderConfirmationEmail(
+                orderConfirmation.customer().email(),
+                customerName,
+                orderConfirmation.totalAmount(),
+                orderConfirmation.orderReference(),
+                orderConfirmation.products()
+        );
     }
 
 }
--- services/notification/src/main/java/com/eltonmessias/ecommerce/kafka/order/Customer.java
+++ services/notification/src/main/java/com/eltonmessias/ecommerce/kafka/order/Customer.java
@@ -1,4 +1,10 @@
 package com.eltonmessias.ecommerce.kafka.order;
 
-public class Customer {
+public record Customer(
+        String id,
+        String firstname,
+        String lastname,
+        String email
+) {
+
 }
